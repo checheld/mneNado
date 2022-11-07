917fa2904b0fe1187c552fef6cea3ef0f8dce58c
Alisa-Grey <aleeloosun@mail.ru>
2022-11-07 22:01:55 +0300

    add dadata

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import NavBar from '../NavBar/Index';
-<<<<<<< HEAD
-=======
-import Footer from '../Footer/Index';
->>>>>>> main
 import './style.sass';
 
 interface ILayout {
@@ -18,7 +14,6 @@ const Layout: React.FC<ILayout> = ({ children, dataAttr, className }) => {
 	return (
 		<Box className={`layout ${className}`} aria-label={dataAttr}>
 			<NavBar />
-			<Footer />
 			{children}
 		</Box>
 	);
